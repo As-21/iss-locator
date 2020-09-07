24e3fb08bd100fe0ed6831473530b7287e4782ff
As-21 <as_21@live.com>
2020-09-07 17:29:24 +0300

    Updates

---
+++ precache-manifest.4703fd9b3fe14fa4108967cd2be1b112.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c76bf9bb8e625ad1558e62d4b913635",
+    "url": "/iss-locator/index.html"
+  },
+  {
+    "revision": "4e5ba1b029176d189ae0",
+    "url": "/iss-locator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ad62ee5e39cf90f4944c",
+    "url": "/iss-locator/static/css/main.950e3d8d.chunk.css"
+  },
+  {
+    "revision": "4e5ba1b029176d189ae0",
+    "url": "/iss-locator/static/js/2.9b54b69b.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/iss-locator/static/js/2.9b54b69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad62ee5e39cf90f4944c",
+    "url": "/iss-locator/static/js/main.438bc768.chunk.js"
+  },
+  {
+    "revision": "c9026ca952be245b1311",
+    "url": "/iss-locator/static/js/runtime-main.e096ce8f.js"
+  },
+  {
+    "revision": "7501ae84b3c073acfb44fcc89819e5d3",
+    "url": "/iss-locator/static/media/ISS_about_picture.7501ae84.jpg"
+  },
+  {
+    "revision": "252650447e8c82292e21ee92416251cf",
+    "url": "/iss-locator/static/media/TNG_Credits.25265044.ttf"
+  },
+  {
+    "revision": "bde71e949046476cea3b6bf0cd795426",
+    "url": "/iss-locator/static/media/TNG_Title.bde71e94.ttf"
+  }
+]);
